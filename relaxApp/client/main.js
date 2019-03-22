3d4fc0c0f0a0fd041a0d2ecb8d33436e2d5304a4
Melinda <femminis.mda@gmail.com>
2019-03-22 21:35:37 +0100

    Premiers tests
    Mise en page + bouton cliquable avec client/main.html, main.js et main.css.
    Essai raté d'ajout d'une image/logo.

--- relaxApp/client/main.js
+++ relaxApp/client/main.js
@@ -3,20 +3,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
+Template.startButton.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    document.getElementById('startButton').style.cssText = 'background-color:pink;';
   },
 });
