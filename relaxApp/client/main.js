167460ad79a4f6f725ce6e4006247dc07353f293
Kykka90 <44426484+Kykka90@users.noreply.github.com>
2019-04-01 21:19:44 +0200

    Ajout des templates pour le menu

--- relaxApp/client/main.js
+++ relaxApp/client/main.js
@@ -2,10 +2,10 @@
 import './main.html';
 import './main.css';
 import '../imports/ui/activerCommencer.js'
+//import '../imports/ui/pageMenu.js'
 
 Template.startButton.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
     document.getElementById('startButton').style.cssText = 'background-color:pink;';
   },
 });
---
+++ relaxApp/imports/ui/pageMenu.js
@@ -0,0 +1,4 @@
+// Importer tous les fichers externes
+import './pageMenu.html';
+import './scripts/menuScript.js';
+
